3931e357d0f81668fd4cf156b7f0366503964706
AlirezaTalebizadeh <94745174+Alirezat78@users.noreply.github.com>
2023-03-31 09:30:55 +0430

    crteate a weather-app for you with api and react

--- src/Weather-app/Weather.js
+++ src/Weather-app/Weather.js
@@ -9,11 +9,73 @@ class App extends React.Component {
         super()
 
         this.state = {
-            
+            apiData: {
+                url: 'https://api.openweathermap.org/data/2.5/weather?q=',
+                key: '26c4d8ad14b57209671494df9bd9fcb9'
+            },
+            timeUser: null,
+            inputElem: '',
+            data: [],
+            continueFlag: false
+
         }
+
+        this.showDate = this.showDate.bind(this)
     }
 
+    //Show user time
+    showDate() {
+        let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
+        let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+        let timeUser = new Date()
+        console.log(timeUser);
+        let now = {
+            day: days[timeUser.getDay()],
+            month: months[timeUser.getMonth()],
+            year: timeUser.getFullYear(),
+            date: timeUser.getDate(),
+        }
+        let time = `${now.date} ${now.day} ${now.month} ${now.year}`
+        this.setState({
+            timeUser: time
+        })
+    }
+
+
+    //fetch data from api
     componentDidUpdate() {
+        fetch(`${this.state.apiData.url}${this.state.inputElem}&appid=${this.state.apiData.key}`)
+            .then(res => res.json()) //convert type of data
+            .then(data => {
+                this.saveData(data)
+                //recieve data and send this to show it
+            })
+    }
+
+    //save data of api to show
+    saveData(data) {
+        let dataOfCity = {
+            name: data.name,
+            country: data.sys.country,
+            temp: (Math.floor(data.main.temp) - 273),
+            main: data.weather[0].main,
+            temp_min: (Math.floor(data.main.temp_min) - 273),
+            temp_max: (Math.floor(data.main.temp_max) - 273)
+        }
+        this.setState({
+            data: dataOfCity,
+            continueFlag: true
+        })
+    }
+
+
+    //get value of input
+    valueHandler(event) {
+        this.setState({
+            inputElem: event.target.value
+        })
     }
 
     render() {
@@ -21,19 +83,24 @@ class App extends React.Component {
         return (
             <div className="app-wrap">
                 <header>
-                    <input type="text" autocomplete="off" className="search-box" placeholder="Search for a city..." />
+                    <input type="text" onChange={this.valueHandler.bind(this)} value={this.state.inputElem} autocomplete="off" className="search-box" placeholder="Search for a city..." />
                 </header>
-                <main>
-                    <section className="location">
-                        <div className="city">Northampton, GB</div>
-                        <div className="date">Thursday 10 January 2020</div>
-                    </section>
-                    <div className="current">
-                        <div className="temp">15<span>°c</span></div>
-                        <div className="weather">Sunny</div>
-                        <div className="hi-low">13°c / 16°c</div>
-                    </div>
-                </main>
+                {!this.state.continueFlag && this.state.data.length === 0 && (<h1>please type your country</h1>)}
+                {this.state.continueFlag && this.state.data && (
+                    <main>
+                        <section className="location">
+                            <div className="city">{this.state.data.country}</div>
+                            <div className="date">{this.showDate}</div>
+                        </section>
+                        <div className="current">
+                            <div className="temp">{this.state.data.temp}<span>°c</span></div>
+                            <div className="weather">{this.state.data.main}</div>
+                            <div className="hi-low">{this.state.data.temp_max} °c / {this.state.data.temp_min} °c</div>
+                        </div>
+                    </main>
+                )}
+
+
             </div>
         );
     }
